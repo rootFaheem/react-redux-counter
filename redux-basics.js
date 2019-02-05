9d9b793e0a5c83eb36174c606ecc4ee3358d656d
Faheem <faheemtech1@gmail.com>
2019-02-06 02:26:54 +0530

    dispatching actions in redux

--- redux-basics.js
+++ redux-basics.js
@@ -2,8 +2,26 @@ const redux = require("redux");
 
 const createStore = redux.createStore;
 
+const initialState = {
+  counter: 0
+};
+
 // Reducer
-const rootReducer = (state, action) => {
+const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
+
   return state;
 };
 
@@ -12,5 +30,8 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+console.log(store.getState());
 
 // Subscription
