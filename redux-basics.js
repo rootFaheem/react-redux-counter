18b39283722a12b5c434c1d66b41d16934dc3faa
Faheem <faheemtech1@gmail.com>
2019-02-06 02:33:09 +0530

    subscription added to the redux

--- redux-basics.js
+++ redux-basics.js
@@ -27,11 +27,14 @@ const rootReducer = (state = initialState, action) => {
 
 // Store
 const store = createStore(rootReducer);
-console.log(store.getState());
+// console.log(store.getState());
+
+// Subscription
+store.subscribe(() => {
+  console.log("[Subscription]", store.getState());
+});
 
 // Dispatching Action
 store.dispatch({ type: "INC_COUNTER" });
 store.dispatch({ type: "ADD_COUNTER", value: 10 });
-console.log(store.getState());
-
-// Subscription
+// console.log(store.getState());
