553d074c7f393ef315badba17aaa045012043315
Faheem <faheemtech1@gmail.com>
2019-02-07 01:48:08 +0530

    changing state immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -53,6 +53,15 @@ class Counter extends Component {
           label="Subtract 15"
           clicked={this.props.onSubtractCounter}
         />
+        <hr />
+        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <ul>
+          {this.props.storedResults.map(strResult => (
+            <li key={strResult.id} onClick={this.props.onDeleteResult}>
+              {strResult.value}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
@@ -60,7 +69,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-    ctr: state.counter
+    ctr: state.counter,
+    storedResults: state.results
   };
 };
 
@@ -69,7 +79,9 @@ const mapDispatchToProps = dispatch => {
     onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
     onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
     onAddCounter: () => dispatch({ type: "ADD", val: 10 }),
-    onSubtractCounter: () => dispatch({ type: "SUBTRACT", val: 15 })
+    onSubtractCounter: () => dispatch({ type: "SUBTRACT", val: 15 }),
+    onStoreResult: () => dispatch({ type: "STORE_RESULT" }),
+    onDeleteResult: () => dispatch({ type: "DELETE_RESULT" })
   };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,24 +1,33 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return {
-        counter: state.counter + 1
-      };
+      const newState = Object.assign({}, state);
+      newState.counter = state.counter + 1;
+      return newState;
     case "DECREMENT":
       return {
+        ...state,
         counter: state.counter - 1
       };
     case "ADD":
       return {
+        ...state,
         counter: state.counter + action.val
       };
     case "SUBTRACT":
       return {
+        ...state,
         counter: state.counter - action.val
+      };
+    case "STORE-RESULT":
+      return {
+        ...state,
+        results: state.results.concat({ id: new Date(), value: state.counter })
       };
     default:
   }
