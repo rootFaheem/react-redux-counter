e6dbd6a8fc74fe7c2aa6eb504e4a51750067fad5
Faheem <faheemtech1@gmail.com>
2019-02-08 01:52:49 +0530

    making result reducers more leaner

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,6 +5,13 @@ const initialState = {
   results: []
 };
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    result => result.id !== action.resultElId
+  );
+  return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
@@ -15,13 +22,7 @@ const reducer = (state = initialState, action) => {
         })
       });
     case actionTypes.DELETE_RESULT:
-      // const id = 2;
-      // const newArray = [...state.results];
-      // newArray.splice(id, 1)
-      const updatedArray = state.results.filter(
-        result => result.id !== action.resultElId
-      );
-      return updateObject(state, { results: updatedArray });
+      return deleteResult(state, action);
     default:
   }
   return state;
