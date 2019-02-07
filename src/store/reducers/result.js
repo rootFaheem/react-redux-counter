50a4d7f8175e65c217bc424d42d85130c776e2c6
Faheem <faheemtech1@gmail.com>
2019-02-07 23:27:39 +0530

    redux devtools

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,27 +1,30 @@
-import * as actionTypes from '../actions';
+import * as actionTypes from "../actions";
 
 const initialState = {
-    results: []
+  results: []
 };
 
-const reducer = ( state = initialState, action ) => {
-    switch ( action.type ) {
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result})
-            }
-        case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1)
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }
-    }
-    return state;
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({ id: new Date(), value: action.result })
+      };
+    case actionTypes.DELETE_RESULT:
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1)
+      const updatedArray = state.results.filter(
+        result => result.id !== action.resultElId
+      );
+      return {
+        ...state,
+        results: updatedArray
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
