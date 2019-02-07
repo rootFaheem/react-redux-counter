4b30d3687be41957f083327d4b9b495e1aa768b1
Faheem <faheemtech1@gmail.com>
2019-02-07 23:59:22 +0530

    thunk passed with logger in applyMiddleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
 import counterReducer from "./store/reducers/counter";
 import resultReducer from "./store/reducers/result";
@@ -29,7 +30,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(logger))
+  composeEnhancers(applyMiddleware(logger, thunk))
 );
 
 ReactDOM.render(
