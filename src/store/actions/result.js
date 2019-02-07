b029f290e1f33a8bc219192a8a628b086ee0701e
Faheem <faheemtech1@gmail.com>
2019-02-08 01:18:48 +0530

    action creator and get state

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -7,8 +7,10 @@ export const saveResult = res => {
   };
 };
 export const storeResult = res => {
-  return dispatch => {
+  return (dispatch, getState) => {
     setTimeout(() => {
+      const oldCounter = getState().ctr.counter;
+      console.log(oldCounter);
       dispatch(saveResult(res));
     }, 2000);
   };
